b3921026aa9648bd61ef92551cd37d84e0f7258c
Filipe Silva <emanuel_filipe@yahoo.com.br>
2024-10-19 15:00:52 -0300

    unit tests with jest

---
+++ frontend/tests/unit/solar-button.spec.ts
@@ -0,0 +1,37 @@
+import { shallowMount } from "@vue/test-utils";
+import SolarButton from "@/components/SolarButton.vue";
+
+describe('SolarButton.vue', () => {
+    it('displays text in default slot position', () => {
+        const wrapper = shallowMount(SolarButton, {
+            propsData: {},
+            slots: {
+                default: 'click here!'
+            }
+        })
+        expect(wrapper.find('button').text()).toBe('click here!')
+    })
+
+    it('has underlying disabled button when disabled:true passed as prop', () => {
+        const wrapper = shallowMount(SolarButton, {
+            propsData: {
+                disabled: true
+            },
+            slots: {
+                default: ''
+            }
+        })
+        expect(wrapper.find('input:disabled'))
+    })
+    it('has no disabled on button when disabled: false passed as prop', () => {
+        const wrapper = shallowMount(SolarButton, {
+            propsData: {
+                disabled: true
+            },
+            slots: {
+                default: ''
+            }
+        })
+        expect(!wrapper.find('input:disabled'))
+    })
+})
